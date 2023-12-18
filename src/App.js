674c4aa47f79afba59a5839cbc7c82231e0621b6
Ayan <ayan2572006@gmail.com>
2023-12-18 13:41:29 +0530

    product card with rating star

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import {BrowserRouter,Routes,Route, Link} from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen";
+import  Navbar  from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import  Container  from 'react-bootstrap/Container';
 export default function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className="d-flex flex-column site-container">
       <header>
-        <Link to="/">amazona</Link>
+        <Navbar variant="dark">
+          <Container>
+            <LinkContainer to="/">
+<Navbar.Brand>amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
-      </div>
+      <main>
+        <Container>
         <Routes>
           <Route path='/' element={<HomeScreen/>}/>
           <Route path='/product/:slug' element={<ProductScreen/>}/>
           </Routes>
-       
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+        </div>
     </BrowserRouter>
   );
 }
